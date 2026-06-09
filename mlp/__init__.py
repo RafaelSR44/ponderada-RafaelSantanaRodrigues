db957114cde96d97ffc91417aecc1601792b4171
Rafael Santana Rodrigues <138138154+RafaelSR44@users.noreply.github.com>
2026-06-09 15:14:56 -0300

    chore: estrutura inicial do projeto (diretórios e requirements.txt)

---
+++ mlp/__init__.py
@@ -0,0 +1,4 @@
+from .network import MLP
+from .activations import relu, relu_derivative, sigmoid, sigmoid_derivative, softmax
+from .losses import cross_entropy, cross_entropy_softmax_gradient
+from .optimizers import SGD, Adam
